90caf06347d83f408308ed9766ea1ef567a96a94
caquli1 <aliwkaplanet66@gmail.com>
2025-05-12 17:35:02 +0300

    update
    I updated design.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import './App.css';
 
 export function Api(city){
   const apiKey = 'd2cf8c474c0287428574ff6a4d643699'; 
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  return axios.get(apiUrl);
+  const apiUrl2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  return Promise.all([axios.get(apiUrl), axios.get(apiUrl2)]);
 }
 
 function App() {
   const [city, setCity] = useState(''); 
   const [weatherData, setWeatherData] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleCityChange = (event) => {
     setCity(event.target.value);
@@ -22,41 +24,71 @@ function App() {
     Api(city)
       .then(response => {
         setWeatherData(response.data);
+        setForecast(response.data);
       })
       .catch(error => {
         console.error(error);
       });
   }, [city]);
 
+  const formatDate = (date) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date * 1000).toLocaleDateString('en-US', options);
+  };
+
 
   return (
-    <div>
+    <div className='App'>
       <div className='search'>
         <TextField id="outlined-basic" variant="outlined" value={city} onChange={handleCityChange} sx={{ bgcolor: "white" }}/>
         <Button variant="contained" color="primary" onClick={fetchData}>Search</Button>
       </div>
 
-    <div className="App">
-      <div>
+    <div>
       {weatherData && (
-        <div>
-          <h1>{weatherData.name}, {weatherData.sys.country}</h1>
-          <h1>{weatherData.main.temp}°C</h1>
-          <p>{weatherData.weather[0].description}</p>
+        <div className='current-weather'>
+          <p style={{fontSize: "50px", marginBottom: "0px"}}>{weatherData.main.temp}°C</p>
+          <p style={{fontSize: "50px", marginBottom: "0px", textTransform: "capitalize"}}>{weatherData.weather[0].description}</p>
+          <p style={{fontSize: "50px", marginBottom: "0px"}}>{weatherData.name}, {weatherData.sys.country}</p>
+        </div>
+      )}
+    </div>
 
-          <div className='weather-info'>
-            <div className='weather-item'>
-              <h3>Humidity</h3>
-              <p className='value'>{weatherData.main.humidity}%</p>
+    <div className='parent'>
+      <div className='highlights-forecast'>
+        <div><p style={{fontSize: "30px", marginBottom: "0px"}}>Today's Highlights</p></div>
+
+        <div className='highlight'>
+          <div className='highlight-info'>
+            <div><img src={`https://img.icons8.com/?size=100&id=EWjiSSGn5H9O&format=png&color=1A1A1A`} style={{width: "40px", height: "40px"}} /></div>
+
+            <div className='info1'>
+              <p style={{fontSize: "20px", marginBottom: "0px"}}>Wind</p>
+              {weatherData && <p style={{fontSize: "20px", marginBottom: "0px"}}>{weatherData.wind.speed} m/s</p>}
             </div>
 
-            <div className='weather-item'>
-              <h3>Wind</h3>
-              <p className='value'>{weatherData.wind.speed} Km/h</p>
+            <div><img src={`https://img.icons8.com/?size=100&id=LDpqdHkI9ueq&format=png&color=1A1A1A`} style={{width: "40px", height: "40px"}} /></div>
+
+            <div className='info1'>
+              <p style={{fontSize: "20px", marginBottom: "0px"}}>Humidity</p>
+              {weatherData && <p style={{fontSize: "20px", marginBottom: "0px"}}>{weatherData.main.humidity} %</p>}
             </div>
           </div>
         </div>
-      )}
+
+        <div className='forecast'>
+          <p style={{fontSize: "30px", marginBottom: "0px"}}>5-Day Forecast</p>
+
+          <div className="forecast-row">
+            {forecast && forecast.list.map((item, index) => (
+              <div className="forecast-item" key={index}>
+                <p style={{fontSize: "20px", marginBottom: "0px"}}>{formatDate(item.dt)}</p>
+                <p style={{fontSize: "20px", marginBottom: "0px"}}>{item.main.temp}°C</p>
+                <p style={{ fontSize: "20px", textTransform: "capitalize" }}></p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
    </div>
